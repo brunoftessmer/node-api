dc1a6288c043b1d072fca747a99fd144169adcb9
Bruno Falcão <brunoftessmer@gmail.com>
2020-09-10 21:47:38 -0300

    db-connect

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//iniciando app
 const app = express();
 
+//iniciando db
+mongoose.connect('mongodb://localhost:27017/node-api',{useNewUrlParser:true});
+require('./src/models/products.js');
+//rota
 app.get('/',(req,res) => {
-  res.send('Hello World dos guri');
+  //res.send('Hello Worldzada');
 
 });
 
