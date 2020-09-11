e07e9336a379255c875487372b5137059d1fe851
Bruno Falcão <brunoftessmer@gmail.com>
2020-09-10 22:00:09 -0300

    product rote

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
 
 //iniciando app
 const app = express();
 
 //iniciando db
 mongoose.connect('mongodb://localhost:27017/node-api',{useNewUrlParser:true});
-require('./src/models/products.js');
+requireDir('./src/models/');
+const Product = mongoose.model('Product');
+
 //rota
 app.get('/',(req,res) => {
-  //res.send('Hello Worldzada');
+  Product.create({
+    title: 'React Native',
+    description: 'É os guri do beck',
+    url: 'http://github.com/facebook/react-native',
+  });
 
 });
 
