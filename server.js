fdfbbaa4768fca3bf38d5d7c3369bcddcc2245b6
Bruno Falcão <brunoftessmer@gmail.com>
2020-09-12 20:22:38 -0300

    crud

--- server.js
+++ server.js
@@ -3,26 +3,19 @@ const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 
-//iniciando app
+//iniciando o app
 const app = express();
+app.use(express.json());
 
-//iniciando db
-mongoose.connect('mongodb://localhost:27017/Docker',{useNewUrlParser: true, useUnifiedTopology: true});
+//iniciando o db
+mongoose.connect(
+    'mongodb://localhost:27017/Docker',
+    { useNewUrlParser: true, useUnifiedTopology: true}
+);
 
-requireDir('./src/models/');
+requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-//rota
-app.get('/',(req,res) => {
-  Product.create({
-    title: 'React Native',
-    description: 'É os guri do beck',
-    url: 'http://github.com/facebook/react-native',
-  });
-
-  return res.send("Hello World");
-
-});
+//rotas
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
