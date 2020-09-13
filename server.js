9220157ac631048f3c611c71cdef5142e0187c9d
Bruno Falcão <brunoftessmer@gmail.com>
2020-09-12 21:16:39 -0300

    added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const requireDir = require('require-dir');
 
 
 //iniciando o app
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //iniciando o db
 mongoose.connect(
