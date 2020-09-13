64c658c16f25f2ecc2964996935d93f5a9b00c69
Bruno Falcão <brunoftessmer@gmail.com>
2020-09-12 21:12:39 -0300

    paginate

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const mongoosePaginate = require('mongoose-paginate');
 
 const ProductSchema = new mongoose.Schema({
   title:{
@@ -22,4 +22,5 @@ const ProductSchema = new mongoose.Schema({
 
 });
 
+ProductSchema.plugin(mongoosePaginate);
 mongoose.model('Product',ProductSchema);
